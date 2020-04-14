898cfdd5ba735d82d0d9580efac6b9e95b8f3780
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-14 16:59:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,12 @@
 # code your #valid_move? method here
 
+#Method with input position as an int
+def valid_move(position)
+  if(position > 0 && position < 8)
+    return true
+  else
+    return false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
