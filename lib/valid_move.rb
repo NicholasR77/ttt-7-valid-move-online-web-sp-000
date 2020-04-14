5a4594cd439199af2a2349e9864a79708422f6e2
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-14 17:12:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,4 +15,4 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-valid_move?([" ", " ", "X", " ", " ", " ", " ", "O", " "], 9)
+valid_move?([" ", " ", "X", " ", " ", " ", " ", "O", " "], 0)
